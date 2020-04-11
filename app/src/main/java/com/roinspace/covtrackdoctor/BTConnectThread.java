f93765544bec439f2fb7b334ecc6cf0b145571fc
Ion Mihnea <ion.mihnea.eduard@gmail.com>
2020-04-11 17:54:46 +0300

    add LICENSE

--- app/src/main/java/com/roinspace/covtrackdoctor/BTConnectThread.java
+++ app/src/main/java/com/roinspace/covtrackdoctor/BTConnectThread.java
@@ -1,3 +1,21 @@
+/*
+ * CovTrack - an app logging Bluetooth devices in your vicinity to monitor infection progress of COVID-19
+ * Copyright (C) 2020  Romanian InSpace Engineering
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
 package com.roinspace.covtrackdoctor;
 
 import android.bluetooth.BluetoothAdapter;
